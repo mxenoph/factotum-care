f1e17fe61a6a2c57e8f4ab74aea318ac15642617
mxenoph <mxenoph@cing.ac.cy>
2020-04-26 09:50:12 +0300

    Saving accordion again due to revert changes.

--- webpack-react-tutorial/src/App.js
+++ webpack-react-tutorial/src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Navigation from "./js/components/Navigation";
 import HomePage from './js/pages/home'
+import Productivity from './js/pages/Productivity'
 
 class App extends Component {
     render() {
       return (
         <div className="App">
-          <HomePage/>   
+          <Productivity/>   
         </div>
       )
     }
-  }
 }
 
 export default App;
