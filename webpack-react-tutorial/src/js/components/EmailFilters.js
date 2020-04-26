d533c5b725c88d2c24438e8a4d2e5aac66667686
mxenoph <mxenoph@cing.ac.cy>
2020-04-26 14:31:24 +0300

    Adding steps to add email filter; we will need to write filters and have them import them in the future

--- webpack-react-tutorial/src/js/components/EmailFilters.js
+++ webpack-react-tutorial/src/js/components/EmailFilters.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
@@ -37,13 +38,22 @@ export default function ControlledExpansionPanels() {
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography className={classes.heading}>General settings</Typography>
-          <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+          <Typography className={classes.heading}>Email filters</Typography>
+          <Typography className={classes.secondaryHeading}>At the moment we only provide information on Gmail filter creation</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-            maximus est, id dignissim quam.
+          <Typography
+              variant="body1"
+              align="left"
+              color="textPrimary"
+              paragraph
+          >
+              <li><span>Go to your Gmail</span></li>
+              <li><span>Navigate to Settings</span></li>
+              <li><span>Select Filters and Blocked Addresses and then Create new filter</span></li>
+              <li><span>Form a filter to distinguish emails only sent to you by providing the following to the To fields: me AND -*</span></li>
+              <li><span>Select Create filter, then OK and select Always mark it as important and categorize as Primary.</span></li>
+              <li><span>Confirm the filter creation</span></li>
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
@@ -53,49 +63,34 @@ export default function ControlledExpansionPanels() {
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
-          <Typography className={classes.heading}>Users</Typography>
+          <Typography className={classes.heading}>Synchronize your email at the beginning of your pomodoro</Typography>
           <Typography className={classes.secondaryHeading}>
-            You are currently not an owner
+             At the moment we only provide steps to alter the syncronization interval for Apple Mail
           </Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-            diam eros in elit. Pellentesque convallis laoreet laoreet.
+          <Container maxWidth="lg">
+          <Typography
+              variant="body1"
+              align="left"
+              color="textPrimary"
+              paragraph
+          >
+              <li><span>Go to your Apple Mail</span></li>
+              <li><span>Navigate to Prefernces from the Menu</span></li>
+              <li><span>Under general set "Check for new messages" to every 30 minutes</span></li>
           </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography className={classes.heading}>Advanced settings</Typography>
-          <Typography className={classes.secondaryHeading}>
-            Filtering has been entirely disabled for whole web server
+          <Typography
+              variant="body1"
+              align="left"
+              color="textPrimary"
+              paragraph
+          >
+              The steps can be taken just one time at the start of your work day and provided you
+              practise pomodoro, you will only receive emails at the end of your pomodoro break.
+
           </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-            vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography className={classes.heading}>Personal data</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-            vitae egestas augue. Duis vel est augue.
-          </Typography>
+          </Container>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     </div>
